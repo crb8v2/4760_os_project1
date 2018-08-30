cdb8a12909675b31f762e3cca1df5a8507272a00
Connor <connor@admin.com>
2018-08-29 22:00:14 -0500

    started exercise 3.8, on step 7. Wait(null) function was just implemented

--- main.c
+++ main.c
@@ -1,8 +1,90 @@
-#comment 
+
+
 
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h> // for perror
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main (int argc, char *argv[]) {
+
+    pid_t childpid = 0;
+    int counter1, counter2;
+    int n, k, m;
+
+    // checks for proper number of args
+    if (argc != 4) {
+        fprintf(stderr, "Invalid number of command line args.");
+        return 1;
+    }
+
+    // get command line values
+    n = atoi(argv[1]);
+    k = atoi(argv[2]);
+    m = atoi(argv[3]);
+
+
+    for (counter1 = 1; counter1 < n; counter1++) {
+        sleep(2);
+    if((childpid = fork()))
+        break;
+    }
+
+
+    for (counter2 = 0; counter2 < k; counter2++) {
+
+        wait(NULL);
+
+        fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n", counter1,
+                (long) getpid(), (long) getppid(), (long) childpid);
+        
+        sleep(m);
+    }
+
     return 0;
 }
+
+
+//    // gets pid and ppid
+//    printf("process id: %ld\n", (long)getpid());
+//    printf("parents process id: %ld\n", (long)getppid());
+//
+//    // fork example
+//    int x;
+//     x = 0;
+//     fork();
+//     x = 1;
+//     printf("process: %ld, x: %d\n\n", (long)getpid(), x);
+//
+//
+//     // ex 3.6
+//     // fork and perror
+//     int childpid = fork();
+//     if (childpid == -1) {
+//         perror("Fork Failed.\N");
+//         return 1;
+//     }
+//     if (childpid == 0)
+//         printf("Child at: %ld\n", (long)getpid());
+//     else
+//         printf("Parent at: %ld\n", (long)getpid());
+//
+//     // how does everything from here up work TOGETHER???
+//
+//     //ex 3.7
+//     pid_t childpid;
+//     pid_t mypid;
+//
+//     mypid = getpid();
+//     childpid = fork();
+//
+//     if(childpid == -1) {
+//         perror("Fork Failed.");
+//         return 1;
+//     }
+//     if(childpid == 0)
+//         printf("child: %ld, ID = %ld\n", (long)getpid(), (long)mypid);
+//     else
+//         printf("parent: %ld, ID = %ld\n", (long)getpid(), (long)mypid);
+
